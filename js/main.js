f33155385709327bf39901920b01eeaed37c7f25
Greg Nagle <gregnagle@icloud.com>
2016-02-29 02:00:50 -0500

    start with the animations

--- js/main.js
+++ js/main.js
@@ -2,8 +2,12 @@
 
 $(function(){
 
-
-
-
     
+    setTimeout(function(){
+        $('.load-logo').addClass('mid');
+        $('.load-logo').removeClass('bot');
+        $('.load-logo').toggleClass('opc');
+    }, 500);
+
+
 });
