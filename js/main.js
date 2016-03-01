ba54fb69ec9bee218119b800680199fdaf33709b
Greg Nagle <gregnagle@icloud.com>
2016-02-29 23:08:27 -0500

    played with initial load animations some more

--- js/main.js
+++ js/main.js
@@ -8,21 +8,42 @@ $(function(){
     //     $('.load-logo').removeClass('bot');
     //     $('.load-logo').toggleClass('opc');
     // }, 500);
-    var $count = 0;
 
     setTimeout(function(){
         $('.load-logo').addClass('mid');
 
         setTimeout(function(){
-            $('.loading').addClass('up');
-        }, 500);
+            $('.loading').addClass('up opc');
+        }, 1000);
 
         setTimeout(function(){
             $('.loading span').addClass('start');
             var loadbar = setInterval(function(){
                 $('.loading span').toggleClass('start');
             }, 3000);
-        }, 2000);
+        }, 2500);
     }, 500);
+
+
+    var count = 0;
+    var loadflag = true;
+
+    $(window).load(function(){ loadflag = false; });
+
+    function makeitcount() {
+        var mycount = setInterval(function(){
+            count++;
+            console.log(count);
+            if ( count >= 4 && !loadflag ) {
+                clearInterval(mycount);
+                $('.loading').removeClass('opc');
+                setTimeout(function(){
+                    $('.load-logo').addClass('top');
+                }, 1500);
+            }
+        }, 1000);
+    }
+
+    setTimeout(makeitcount, 2500);
 
 });
