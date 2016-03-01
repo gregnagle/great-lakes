5e25f7d978bbbc705f6da692f0699421cb1752db
Greg Nagle <gregnagle@icloud.com>
2016-03-01 01:53:17 -0500

    messed around with the animations

--- js/main.js
+++ js/main.js
@@ -9,35 +9,39 @@ $(function(){
         $loadlogoTop  = $('.load-logo.top');
 
 
+
+
+
     var measureit = function(){
 
         var
             $lfHght    = $loadframe.outerHeight(),
             $llHght    = $loadlogo.outerHeight(),
-            $llmidMth  = ($lfHght / 2) - ($llHght / 2),
+            $llmidMth  = Math.floor( (($lfHght / 2) - ($llHght / 2)) + 30 ),
             $lltopMth  = $lfHght - $llHght;
-            // $boom = Math.floor($llmidMth)
-
-
-        $loadlogoMid.css({
-            'bottom' : $llmidMth
-        });
-        $loadlogoTop.css({
-            'bottom' : $lltopMth
-        });
+            // $boom = Math.floor($llmidMth);
 
 
 
-        // console.log($wndwdth);
-        // console.log($wndhgth);
-        // console.log($llHght);
-        // console.log($lfHght);
-        // console.log($llmidMth);
-        // console.log($lltopMth);
+
+            $loadlogo.addClass('opc')
+            $('.load-logo').css({
+                'bottom' : $llmidMth
+            });
+
+
+        console.log($llHght);
+        console.log($lfHght);
+        console.log($llmidMth);
+        console.log($lltopMth);
 
     };
 
-    measureit();
+
+    setTimeout(measureit, 500);
+
+
+
 
     $(window).resize(function(){
         measureit();
@@ -47,41 +51,6 @@ $(function(){
 
 
 
-    // setTimeout(function(){
-    //     $('.load-logo').addClass('mid');
-    //
-    //     setTimeout(function(){
-    //         $('.loading').addClass('up opc');
-    //     }, 1000);
-    //
-    //     setTimeout(function(){
-    //         $('.loading span').addClass('start');
-    //         var loadbar = setInterval(function(){
-    //             $('.loading span').toggleClass('start');
-    //         }, 3000);
-    //     }, 2500);
-    // }, 500);
-    //
-    //
-    // var count = 0;
-    // var loadflag = true;
-    //
-    // $(window).load(function(){ loadflag = false; });
-    //
-    // function makeitcount() {
-    //     var mycount = setInterval(function(){
-    //         count++;
-    //         console.log(count);
-    //         if ( count >= 4 && !loadflag ) {
-    //             clearInterval(mycount);
-    //             $('.loading').removeClass('opc');
-    //             setTimeout(function(){
-    //                 $('.load-logo').addClass('top');
-    //             }, 1500);
-    //         }
-    //     }, 1000);
-    // }
-    //
-    // setTimeout(makeitcount, 2500);
+
 
 });
